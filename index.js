5c07e8e49afbca1e23199b94802be05017f88f4b
Valentin Gamboa <gamboav13@gmail.com>
2024-07-20 00:11:56 -0300

    Update the index from the Puppeteer example on their homepage

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
-import puppeteer from 'puppeteer';
+import puppeteer from 'puppeteer'
+// Or import puppeteer from 'puppeteer-core';
 
 try {
+  // Launch the browser and open a new blank page
   console.log('TEST RUN ⏱️')
   console.log('Loading Puppeteer launch...')
-  const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox']})
+  const browser = await puppeteer.launch();
   console.log('Loading Puppeteer newPage...')
   const page = await browser.newPage()
-  await page.setCacheEnabled(false)
-  console.log('Loading Puppeteer goto [https://pptr.dev/]...')
-  await page.goto('https://pptr.dev/')
-  console.log('Get Title...')
-  const title = await page.$eval('h1', text => text.textContent)
-  console.log('TEST PASS ✅   ->  [https://pptr.dev/] Title = ', title)
 
-  await browser.close();
+  // Navigate the page to a URL.
+  console.log('Loading Puppeteer goto [https://developer.chrome.com/]...')
+  await page.goto('https://developer.chrome.com/')
+
+  // Set screen size.
+  await page.setViewport({width: 1080, height: 1024})
+
+  // Type into search box.
+  console.log('Fill locator [.devsite-search-field]...')
+  await page.locator('.devsite-search-field').fill('automate beyond recorder')
+
+  // Wait and click on first result.
+  console.log('Click Locator [.devsite-result-item-link]...')
+  await page.locator('.devsite-result-item-link').click();
+
+  // Locate the full title with a unique string.
+  console.log('Wait Handle Locator [text/Customize and automate]...')
+  const textSelector = await page.locator('text/Customize and automate').waitHandle()
+
+  console.log('Get Full Title...')
+  const fullTitle = await textSelector?.evaluate(el => el.textContent)
+
+  // Print the full title.
+  console.error('TEST PASS ✅   ->  The title of this blog post is "%s".', fullTitle)
+
+  await browser.close()
 } catch (error) {
   console.error('TEST ERROR ❌   ->  Error = ', error)
 }
